9db4abb793808c84475a084131481eb850c4beb5
vera2042 <vera2042@yahoo.com>
2015-02-17 00:04:21 -0500

    create functions Assignment 2
    initial pass

---
+++ cachematrix.R
@@ -0,0 +1,35 @@
+## The function makeCasheMatrix takes a matrix as input
+## and sets 
+## 
+
+## Write a short comment describing this function
+
+makeCacheMatrix <- function(x = matrix()) {
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(solve) m <<- solve
+  getinverse <- function() m
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
+}
+
+
+## The function cacheSolve 
+
+cacheSolve <- function(x, ...) {
+        ## Return a matrix that is the inverse of 'x'
+  m <- x$getinverse()
+  if(!is.null(m)) {
+    message("getting cashed data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data,...)
+  x$setinverse(m)
+  m
+}
